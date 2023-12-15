75f09a02c1c734fdae12d5bb6f5fb960500c84cb
leoferson07 <129422909+leoferson07@users.noreply.github.com>
2023-12-15 13:11:20 -0400

    terminado

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
 const server = require("./app");
 const PORT = 3001;
+const { conn } = require("./DB_connection");
 
-
-
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+conn.sync({ force: false })
+.then(()=> {
+      server.listen(PORT, () => {
+      console.log('Server raised in port: ' + PORT);
+   });
+})
+.catch(error => console.log(error.message))
